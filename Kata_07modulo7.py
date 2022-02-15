ae20b4e8325fba4a34a888aefb8aea1c19d5921f
Angel2Dm <99290672+Angel2Moreno@users.noreply.github.com>
2022-02-15 13:44:37 -0600

    addKatas 7-9

---
+++ Kata_07modulo7.py
@@ -0,0 +1,16 @@
+#1
+new_planet = ''
+planets = []
+
+new_planet = input('Escribe')
+while new_planet =='done':
+    print(new_planet)
+    break
+
+#2
+lista=[]
+for i in range(9):
+    datos =(input('ingresa los planetas '))
+    lista.append(datos)
+    print(lista)
+      
